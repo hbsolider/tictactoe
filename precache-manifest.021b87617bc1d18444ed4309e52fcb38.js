2ed22144d80c5137221e06851d13e80d82c8ba2e
HoaiBao <rorolo2011@gmail.com>
2020-10-06 01:54:09 -0600

    Updates

---
+++ precache-manifest.021b87617bc1d18444ed4309e52fcb38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32079b71463c9e9ecbf091250019f85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "175294b20ec1d3af1e46",
+    "url": "/static/css/main.05e3660d.chunk.css"
+  },
+  {
+    "revision": "18ff4b55b7864a6c49f5",
+    "url": "/static/js/2.5d344039.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d344039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175294b20ec1d3af1e46",
+    "url": "/static/js/main.a90ac1e7.chunk.js"
+  },
+  {
+    "revision": "df40ba753f8222cfb8ed",
+    "url": "/static/js/runtime-main.0cd3ba68.js"
+  }
+]);
